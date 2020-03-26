2ff7fb9d5ccedc3edcf90ef63c2301450eba524c
Konstantin Pristavka <konstantin.pristavka@ekulus.com>
2020-03-26 22:22:58 +1100

    feat(tutorial): complete step 4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
 	production: false,
-	githubPersonalAccessToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	githubPersonalAccessToken: '8a1f007fa0c64b933ea0ee3913433a9add2652a2'
 };
